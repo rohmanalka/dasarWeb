60b34a7583c598d429f3d38bb8ea690f20b5668a
rohmanalka <rohmanalka06@gmail.com>
2024-09-25 14:28:52 +0700

    JS5-PHP2 Praktikum4 nilai default

--- JS5-PHP2/function.php
+++ JS5-PHP2/function.php
@@ -1,5 +1,5 @@
 <?php
-    function perkenalan($nama, $salam) {
+    function perkenalan($nama, $salam="Assalamualaikum") {
         echo $salam . ", ";
         echo "Perkenalkan, nama saya " . $nama . "<br/>";
         echo "Senang berkenalan dengan Anda<br/>";
@@ -10,5 +10,5 @@
     $saya = "Elok";
     $ucapanSalam = "Selamat pagi";
 
-    perkenalan($saya, $ucapanSalam);
+    perkenalan($saya);
 ?>
