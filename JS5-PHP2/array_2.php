85ec9a0b2b8bba1d7675a391546a7e0d28b3c8d0
rohmanalka <rohmanalka06@gmail.com>
2024-09-25 14:16:12 +0700

    JS5-PHP2 Praktikum2

---
+++ JS5-PHP2/array_2.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title></title>
+        <style>
+        table {
+            border-collapse: collapse;
+        }
+
+        td {
+            border: 1px solid;
+            text-align: left;
+            padding: 8px;
+        }
+    </style>
+    </head>
+    <body>
+        <?php
+            $Dosen = [
+                'nama' => 'Elok Nur Hamdana',
+                'domisili' => 'Malang',
+                'jenis_kelamin' => 'Perempuan'
+            ];
+        ?>
+        <table>
+            <tr>
+                <td>Nama</td>
+                <td><?php echo $Dosen['nama']; ?></td>
+            </tr>
+            <tr>
+                <td>Domisili</td>
+                <td><?php echo $Dosen['domisili']; ?></td>
+            </tr>
+            <tr>
+                <td>Jenis Kelamin</td>
+                <td><?php echo $Dosen['jenis_kelamin']; ?></td>
+            </tr>
+        </table>
+    </body>
+</html>
