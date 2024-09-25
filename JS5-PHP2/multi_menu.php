223822893e161bc10d3bf4c15d2cd8ba9bff59d8
rohmanalka <rohmanalka06@gmail.com>
2024-09-25 14:52:32 +0700

    JS5-PHP2 Prak6 sub menu

--- JS5-PHP2/multi_menu.php
+++ JS5-PHP2/multi_menu.php
@@ -35,8 +35,12 @@
 
     function tampilkanMenuBertingkat(array $menu) {
         echo "<ul>";
-        foreach ($menu as $key => $item) {
-            echo "<li>{$item['nama']}</li>";
+        foreach ($menu as $item) {
+            echo "<li>{$item['nama']}";
+            if (isset($item['subMenu'])) {
+                tampilkanMenuBertingkat($item['subMenu']);
+            }
+            echo "</li>";
         }
         echo "</ul>";
     }
