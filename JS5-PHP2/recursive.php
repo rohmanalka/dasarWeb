971bc3137b9390b16f5775ea23a7188a5da19714
rohmanalka <rohmanalka06@gmail.com>
2024-09-25 14:40:29 +0700

    JS5-PHP Prak5

--- JS5-PHP2/recursive.php
+++ JS5-PHP2/recursive.php
@@ -1,8 +1,5 @@
 <?php
-    function tampilkanHaloDunia(){
-        echo "Halo dunia! <br>";
-
-        tampilkanHaloDunia();
+    for($i=1; $i <=25; $i++){
+        echo "Perulangan ke-{$i} <br>";
     }
-    tampilkanHaloDunia();
 ?>
