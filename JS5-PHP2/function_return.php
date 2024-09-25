8e9ed0a7e0f41cfe632c7a6ec394526e9f78f6cb
rohmanalka <rohmanalka06@gmail.com>
2024-09-25 14:33:33 +0700

    JS5-PHP2 Praktikum4 fungsi dalam fungsi

--- JS5-PHP2/function_return.php
+++ JS5-PHP2/function_return.php
@@ -4,5 +4,13 @@
         return $umur;
     }
 
-    echo "Umur saya adalah". hitungUmur(2004, 2023)."tahun"
+    function perkenalan($nama, $salam = "Assalamualaikum") {
+        echo $salam . ", ";
+        echo "Perkenalkan, nama saya " . $nama . "<br/>";
+        
+        echo "Saya berusia " . hitungUmur(1988, 2023) . " tahun<br/>";
+        echo "Senang berkenalan dengan Anda<br/>";
+    }
+
+    perkenalan("Elok")
 ?>
