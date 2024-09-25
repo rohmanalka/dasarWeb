0287df417323303b313d9734a78222eeb8300da4
rohmanalka <rohmanalka06@gmail.com>
2024-09-25 14:17:09 +0700

    JS5-PHP2 Praktikum4

---
+++ JS5-PHP2/function.php
@@ -0,0 +1,10 @@
+<?php
+    function perkenalan(){
+        echo "Assalamualaikum, ";
+        echo "Perkenalan, nama saya Alka<br/>";
+        echo "Senang berkenalan dengan anda<br/>";
+    }
+
+    perkenalan();
+    perkenalan();
+?>  
