8f198ceae290b4c9de569363b0122bdfac78f4cb
rohmanalka <rohmanalka06@gmail.com>
2024-09-25 14:15:23 +0700

    JS5-PHP2 Praktikum1

---
+++ JS5-PHP2/array_1.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    </head>
+    <body>
+        <h2>Array Terindeks</h2>
+        <?php
+            $Listdosen = ["Elok Nur Hamdana", "Unggul Pemenang", "Bagas Nugraha"];
+
+            echo $Listdosen[2] . "<br>";
+            echo $Listdosen[0] . "<br>";
+            echo $Listdosen[1] . "<br>";
+
+            echo "<br>";
+            foreach ($Listdosen as $dosen) {
+                echo $dosen . "<br>";
+            }
+        ?>
+    </body>
+</html>
