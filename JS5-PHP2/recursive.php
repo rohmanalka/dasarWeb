89d7156f835fbe76324d4b546e3e06a8f03858a4
rohmanalka <rohmanalka06@gmail.com>
2024-09-28 19:01:31 +0700

    JS5-PHP2 Prak 5 Rekursif tanpa loop for

--- JS5-PHP2/recursive.php
+++ JS5-PHP2/recursive.php
@@ -1,5 +1,10 @@
 <?php
-    for($i=1; $i <=25; $i++){
-        echo "Perulangan ke-{$i} <br>";
+    function tampilkanAngka(int $jumlah, int $indeks = 1) {
+        echo "Perulangan ke-{$indeks} <br>";
+
+        if($indeks < $jumlah) {
+            tampilkanAngka($jumlah, $indeks + 1);
+        }
     }
+    tampilkanAngka(20);
 ?>
