a83e335ed10a6fca7aabfa42a84eed91819bb1c4
rohmanalka <rohmanalka06@gmail.com>
2024-09-28 18:56:42 +0700

    JS5-PHP2 Prak 9 Entitas HTML

---
+++ JS5-PHP2/entities.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Entities HTML</title>
+</head>
+<body>
+    <p>It&#39; time to read a HTML5 book.</p>
+    <p>&nbsp; &nbsp; &nbsp; &nbsp;Keuntungan dari menggunakan nama entities: <br>
+        &nbsp; &nbsp; &nbsp; &nbsp;sebuah nama entitas mudah diingat. Kerugian dari menggunakan nama entities: <br>
+        &nbsp; &nbsp; &nbsp; &nbsp;Browser mungkin tidak mendukung semua nama entitas, tapi dukungan untuk nomor lebih baik</p>
+    <p>&#169; 2023 jti.com</p>
+</body>
+</html>
