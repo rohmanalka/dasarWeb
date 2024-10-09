48fcac75debecf6079c2b7ae5ca06cf359ae459b
rohmanalka <rohmanalka06@gmail.com>
2024-10-09 15:28:59 +0700

    JS7-PHPFORM Prak 7 form validasi dengan ajax

--- JS7-PHPFORM/form_validasi.php
+++ JS7-PHPFORM/form_validasi.php
@@ -1,49 +1,63 @@
 <!DOCTYPE html>
 <html>
-<head>
-    <title>Form Input dengan Validasi</title>
-    <script src="https://code.jquery.com/jquery-3.6.8.min.js"></script>
-</head>
-<body>
-    <h1>Form Input dengan Validasi</h1>
-    <form id="myForm" method="post" action="proses_validasi.php">
-        <label for="nama">Nama:</label>
-        <input type="text" id="nama" name="nama">
-        <span id="nama-error" style="color: red;"></span><br>
+    <head>
+        <title>Form Input dengan Validasi</title>
+        <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    </head>
+    <body>
+        <h1>Form Input dengan Validasi</h1>
+        <form id="myForm" method="post" action="proses_validasi.php" >
+            <label for="nama">Nama: </label>
+            <input type="text" id="nama" name="nama">
+            <span id="nama-error" style="color:red;"></span>
+            <br>
 
-        <label for="email">Email:</label>
-        <input type="text" id="email" name="email">
-        <span id="email-error" style="color: red;"></span><br>
+            <label for="email">Email:</label>
+            <input type="text" id="email" name="email"> 
+            <span id="email-error" style="color:red;"></span>
+            <br>
 
-        <input type="submit" value="Submit">
-    </form>
+            <input type="submit" value="Submit">
+        </form>
 
-    <script>
-        $(document).ready(function() {
-            $("#myForm").submit(function(event) {
-                var nama = $("#nama").val();
-                var email = $("#email").val();
-                var valid = true;
+        <div id="hasil">
 
-                if (nama === "") {
-                    $("#nama-error").text("Nama harus diisi.");
-                    valid = false;
-                } else {
-                    $("#nama-error").text("");
-                }
+        </div>
 
-                if (email === "") {
-                    $("#email-error").text("Email harus diisi.");
-                    valid = false;
-                } else {
-                    $("#email-error").text("");
-                }
+        <script>
+            $(document).ready(function() {
+                $("#myForm").submit(function(event) {
+                    event.preventDefault(); 
+                    var nama = $("#nama").val();
+                    var email = $("#email").val();
+                    var valid = true;
 
-                if (!valid) {
-                    event.preventDefault(); // Mencegah pengiriman form jika validasi gagal
-                }
+                    if (nama === "") {
+                        $("#nama-error").text("Nama harus diisi.");
+                        valid = false;
+                    } else {
+                        $("#nama-error").text("");
+                    }
+
+                    if (email === "") {
+                        $("#email-error").text("Email harus diisi.");
+                        valid = false;
+                    } else {
+                        $("#email-error").text("");
+                    }
+
+                    if (valid) {
+                        $.ajax({
+                            url: "proses_validasi.php",
+                            type: "POST",
+                            data: $(this).serialize(),
+                            success: function(response) {
+                                $("#hasil").html(response);
+                            }
+                        });
+                    }
+                });
             });
-        });
-    </script>
-</body>
+        </script>
+    </body>
 </html>
