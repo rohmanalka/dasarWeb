2df8af3c113f7df487161b209c24c7f0c9224ba9
rohmanalka <rohmanalka06@gmail.com>
2024-10-09 14:04:35 +0700

    JS7-PHPFORM Prak 5 preg_match angka

--- JS7-PHPFORM/regex.php
+++ JS7-PHPFORM/regex.php
@@ -8,6 +8,14 @@ if(preg_match($pattern, $text)) {
     echo "Tidak ada huruf kecil!<br>";
 }
 
+$pattern = '/[0-9]+/';
+$text = 'There are 123 apples.';
+
+if(preg_match($pattern, $text, $matches)) {
+    echo "Cocokkan: " . $matches[0] . "<br>";
+} else {
+    echo "Tidak ada yang cocok <br>";
+}
 
 
 
