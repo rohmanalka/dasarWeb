e7740e3439f5f15c4f30bb3d5628bd453729cc6e
rohmanalka <rohmanalka06@gmail.com>
2024-10-16 14:36:52 +0700

    JS8-PHPFORMUP Prak 3 Form Ajax Gambar MultiUpload

--- JS8-PHPFORMUP/form_upload_ajax.php
+++ JS8-PHPFORMUP/form_upload_ajax.php
@@ -4,13 +4,13 @@
         <title>Unggah File Dokumen</title>
     </head>
     <body>
-        <form id="upload-form" action="upload_ajax.php" method="post" enctype="multipart/form-data">
-            <input type="file" name="file" id="file">
-            <input type="submit" name="submit" value="Unggah">
+        <form action="upload_ajax.php" id="upload-form" method="post" enctype="multipart/form-data">
+        <input type="file" name="files[]" multiple="multiple" id="file">
+        <input type="submit" name="submit" value="Unggah">
         </form>
         <div id="status"></div>
-
+        
         <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
         <script src="upload.js"></script>
-    </body>
+   </body>
 </html>
